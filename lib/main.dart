2a3d0d7926558ec523532157b84b34a419fbab34
Elcio Lopes da Silva <elciols@gmail.com>
2022-06-25 19:36:26 -0300

    Recebendo o parâmetro da detalhePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'pages/detalhe_page.dart';
+import 'pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,6 +12,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Navegação',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      //o OnGenerateRoute não é chamado quando a rota dele está ativada pelo routes
+      // e tambem quando for feita por Page 'PushNamed' rota nomeada
+      onGenerateRoute: (RouteSettings settings) {
+        if (settings.name == '/') {
+          return MaterialPageRoute(
+            settings: settings,
+            builder: (context) => const HomePage(),
+          );
+        }
+        if (settings.name == '/detalhe') {
+          return MaterialPageRoute(
+            settings: settings,
+            builder: (context) => const DetalhePage(),
+          );
+        }
+        return null;
+      },
+
+      // routes: {
+      //   '/': (_) => const HomePage(),
+      //   '/detalhe': (_) => const DetalhePage(),
+      // },
+    );
   }
 }
---
+++ lib/pages/detalhe_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetalhePage extends StatelessWidget {
+  const DetalhePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var parametro = ModalRoute.of(context)?.settings.arguments as String?;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhe'),
+      ),
+      body: Center(
+        child: Text(parametro ?? 'Não foi enviado o parâmetro'),
+      ),
+    );
+  }
+}
